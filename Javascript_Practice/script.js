52848c14ed02554bf4651337b50e53cb8350a40d
Manisha <manisharani2745@gmail.com>
2024-07-16 09:19:10 +0530

    15thJulyVariables

---
+++ Javascript_Practice/script.js
@@ -0,0 +1,39 @@
+console.log('Hello');
+//var updated, redeclared
+
+//var, let, const
+// var a = 'Hi';
+// var a = 'Hello';
+// var b = 9;
+// console.log(a);
+// console.log(a);
+// console.log(b);
+
+
+
+var ab = 'scope';
+console.log(ab);
+console.log(window.ab);
+
+
+
+//let
+//cant be reclared, can be updated
+
+let c = 'Hello World';
+c = 'init';
+console.log(c);
+console.log(window.c);  //undefined
+
+
+//const
+//cant redeclare, cant update
+
+
+const k = 'variable';
+const l = 'declare';
+console.log(k);
+console.log(window.k); //undefined
+
+
+
