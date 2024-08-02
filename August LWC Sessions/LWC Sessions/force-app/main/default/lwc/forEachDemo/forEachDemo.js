ae0ac7567e3feaa25cde3dc8dbf3a1d1e8196b5e
Manisha <manisharani2745@gmail.com>
2024-08-02 09:38:14 +0530

    2ndAugRenderingListOfItemsConditionally

---
+++ August LWC Sessions/LWC Sessions/force-app/main/default/lwc/forEachDemo/forEachDemo.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachDemo extends LightningElement {
+
+    contacts = [
+        {
+            name :'Anup',
+
+            age :'20',
+            salary :'10000'
+        },
+        {
+            name :'Ram',
+
+            age :'30',
+            salary :'20000'
+        },
+        {
+            name :'Hema',
+
+            age :'40',
+            salary :'30000'
+        }
+    ]
+}
